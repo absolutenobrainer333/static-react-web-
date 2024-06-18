83e133730c3ebf359b860d5076bee6a9bdd925ef
No Brainer <nnnannn710@gmail.com>
2024-06-18 23:57:08 +0700

    change 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h3>i play valorant bro</h3>
     </div>
   );
 }
